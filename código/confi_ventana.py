9e48691880ef8e3a5e537af6537f2ba5189efa72
Wins0202 <winsderlin.p.p@hotmail.com>
2025-03-19 11:36:25 +0100

    importación de librerías y definición del espacio de la ventana

---
+++ código/confi_ventana.py
@@ -0,0 +1,10 @@
+"""Configuración de la ventana donde se va a visualizar el juego"""
+
+screenWidth = 1000 #Ancho de la ventana
+screenHeight = 800 #Altura de la ventana
+
+
+"""Colores que tendrá la pantalla de inicio. Los colores son 
+tomados del código de colores en RGB"""
+black = (0, 0, 0)  
+white = (255, 255, 255)
---
+++ código/fase_1.py
@@ -0,0 +1,19 @@
+""" Programa creado como proyecto para el módulo de IDME. En este proyecto está plasmada la 
+creación del código del tradicional juego arcade de Pac-Man."""
+
+
+#Importción de librerías:
+
+import pygame #Libreria con la que programa el juego. 
+
+import sys #Cuando se cierre la ventana deje de correr la aplicación.
+
+from confi_ventana import * #desde la pestaña creada, importamos todo lo que esté allí escrito.
+
+"""Para la correcta importación de la librería se tiene que descargar con el comando 
+"pip install pygame" y se procederá a descargar la librería para su uso"""
+
+
+
+
+
