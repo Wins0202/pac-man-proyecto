460a41f881dd58ba410aa2f370e6aa940746751c
Wins0202 <winsderlin.p.p@hotmail.com>
2025-03-21 12:57:32 +0100

    Corrección de error para abrir la ventana

---
+++ código/config.py
@@ -0,0 +1,12 @@
+"""Configuración de la ventana donde se va a visualizar el juego"""
+
+#Definción del tamaño de la pantalla:
+width = 800 #Ancho de la ventana
+height = 600 #Altura de la ventana
+
+
+#Colores que tendrá la pantalla de inicio: 
+"""Los colores son tomados del código de colores en RGB"""
+
+negro = (0, 0, 0)  
+blanco = (255, 255, 255)
--- código/main.py
+++ código/main.py
@@ -8,14 +8,17 @@ import pygame #Libreria con la que programa el juego.
 
 import sys #Cuando se cierre la ventana deje de correr la aplicación.
 
-from confi_ventana import * #desde la pestaña creada, importamos todo lo que esté allí escrito.
+from config import * #Importación de la librería creada para las configuraciones
 
 """Para la correcta importación de la librería se tiene que descargar con el comando 
 "pip install pygame" y se procederá a descargar la librería para su uso"""
 
 
-"""Este conjunto de líneas sirve para que se inicie el juego. Definiremos las funciones para el ´
+"""Este conjunto de líneas sirve para que se inicie el juego. Definiremos las funciones para el
 control básico del juego"""
+
+"""Configuración de la ventana donde se va a visualizar el juego"""
+
 
 #Definición de variables:
 
@@ -25,7 +28,7 @@ class Game:
         pygame.init() 
     
         #Se crea la ventana:
-        self.screen = pygame.display.set_mode ((screenWidth, screenHeight)) #activa el ancho y el alto de la ventana que se había importado de la pestaña creada
+        self.screen = pygame.display.set_mode ((width, height)) #activa el ancho y el alto de la ventana que se había importado de la pestaña creada
         pygame.display.set_caption ("PAC-MAN") #Nombre que tendrá la ventanilla cuando se abrá para jugar
 
         #Se establece un reloj para controlar la velocidad del juego
@@ -43,8 +46,27 @@ class Game:
 
     def update(self): 
         #Actualizará las funciones del juego
-        pass #no hay que actulizar, así que se deja en "reposo"
+        pass #no hay que actualizar, así que se deja en "reposo"
+
+    def draw(self): #Dibujar los elementos en la pantalla
+        #La base de la pantalla es color negro
+        self.screen.fill (negro)
+
+        pygame.display.flip() #Actualizar la pantalla sino no se observa
+
+    def run(self): #Bucle principal del juego cuando se llame 'run'
+        while self.running: 
+            self.handleEvents () #Procesar los eventos
+            self.update () #Actualizar el estado del juego
+            self.draw () #Dibujar los elementos que están en el juego
+            self.clock.tick (60) #60FPS
+
+        pygame.quit() #Cerrar pygame al terminar
+        sys.exit() 
 
 
-
+#Creación y ejecución del juego:
+if __name__ == "__main__" : #si lo ejecutamos sólo se va a ejecutar este archivo
+        game = Game ()          #Si ejecutamos desde otro archivo que no sea "Game" no va a correr
+        game.run ()             #Es el que llama a todos los eventos del 
 
